ede57d855f0fbc8bca8f52c74104e4aefbad39ed
Nathan Tucker <abezhat@verizon.net>
2011-06-08 05:15:17 -0400

    early alpha

---
+++ SACodec/Debug/test.bat
@@ -0,0 +1,3 @@
+SACodec -e -f "C:\Users\a\Documents\Visual Studio 2008\Projects\SACodec\Debug\GoodBye.wma"
+SACodec -d -k DEADBEEF -s TucWaterMark1923-2005 -f "C:\Users\a\Documents\Visual Studio 2008\Projects\SACodec\Debug\GoodBye.SAC.wma"
+pause
